44a7b84e841e23dde622bae13a97785a28fab0e9
Laravel Shift <shift@laravelshift.com>
2017-01-18 19:23:35 +0000

    Shift cleanup

--- app/User.php
+++ app/User.php
@@ -8,8 +8,6 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Model;
 
-
-
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
 
