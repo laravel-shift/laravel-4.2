b699f192b2fdcc75d050da0734af76c8c28a73d6
Laravel Shift <shift@laravelshift.com>
2017-01-18 19:11:30 +0000

    Shift user authentication
    Update the `User` model to the new authentication system in Laravel 5.

--- app/User.php
+++ app/User.php
@@ -1,14 +1,14 @@
 <?php
 
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
-class User extends Eloquent implements UserInterface, RemindableInterface
+class User extends Eloquent implements AuthenticatableContract, CanResetPasswordContract
 {
 
-    use UserTrait, RemindableTrait;
+    use Authenticatable, CanResetPassword;
 
     /**
      * The database table used by the model.
@@ -23,4 +23,6 @@ class User extends Eloquent implements UserInterface, RemindableInterface
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    protected $fillable = ['name', 'email', 'password'];
 }
